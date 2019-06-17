86077082c5b8c2a2b1c65f86fa02006e11f58af1
jackbuy <2538362801@qq.com>
2019-06-17 15:35:05 +0800

    init

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,31 @@
 import express from 'express'
 const app = express();
-
 const query = (req: any , res: any): void => {
-    res.send('hello typescript');
+    const a: object = {
+        a: 1,
+        b: 2
+    }
+
+    const b: Array<object> = [{aa: 1, bb:2}]
+
+    const c: string = 'hello typeScript'
+
+    const d: boolean = true
+
+    const e: number = 123
+
+    enum Color {red, blue, green}
+    const f: Color = Color.green
+    
+    const name = (): string => {
+        return 'hello'
+    }
+    
+    res.send(a);
 }
 
 app.get('/', query);
 
-app.listen(10000, () => {
+app.listen(10000, (): void => {
     console.log('服务已启动，端口10000');
 })
